dd251a0282f39b367d15b37c83e9a38b7d2994d8
Adam Hoffman <S00197432@mail.itsligo.ie>
2021-08-12 13:41:37 +0100

    Nearly Finished

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace RepeatLab1OOP
     {
         static void Main(string[] args)
         {
+
+            Song s1 = new Song("Ed Sheeran", "Beautiful People", 3.15, Genre.Pop);
+            Song s2 = new Song("Regard", "Ride It", 3.37, Genre.Dance);
+            Song s3 = new Song("Tones & I", "Dance Monkey", 4.20, Genre.Dance);
+            Song s4 = new Song("Post Malone", "Circles", 3.25, Genre.Pop);
+            Song s5 = new Song("Ed Sheeran", "South Of The Border", 4.26, Genre.Other);
+
+            List<Song> Playlist = new List<Song>();
+
+            Playlist.Add(s1);
+            Playlist.Add(s2);
+            Playlist.Add(s3);
+            Playlist.Add(s4);
+            Playlist.Add(s5);
+
+
         }
     }
 }
---
+++ Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatLab1OOP
+{
+    class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre SongGenre { get; set; }
+       
+        public Song(string title, string artist, double duration, Genre genre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            SongGenre = genre;
+        }
+
+        //prints the title,artist,duration and genre of a song
+        public override string ToString()
+        {
+            return string.Format($"{Artist} {Title} {Duration} {SongGenre}");
+        }
+    }
+    public enum Genre
+    {
+        Rock,
+        Pop,
+        Dance,
+        Other
+    }
+}
